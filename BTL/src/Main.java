2fd3045a0f9f8acc0d0888fb1fe9de5bd77900aa
Raphael9143 <124426130+Raphael9143@users.noreply.github.com>
2023-09-21 15:20:45 +0700

    Delete BTL directory

--- BTL/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Dictionary dictionary = new Dictionary();
-        DictionaryManagement dc = new DictionaryManagement();
-        dc.insertFromComandline();
-        DictionaryCommandline d = new DictionaryCommandline();
-        //d.showAllWords();
-    }
-}
