f1620ed7924e4f4592bc1d6be052bd81ca3ecc5f
Cuong Do <cgdd2511.hd@gmail.com>
2023-10-06 08:29:31 +0700

    FlashCard

--- FavouriteDictionary.java
+++ FavouriteDictionary.java
@@ -1,11 +1,8 @@
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class FavouriteDictionary extends Dictionary{
-
     public FavouriteDictionary() throws IOException {
         super();
         super.FileImport("FavDict.txt");
@@ -26,14 +23,13 @@ public class FavouriteDictionary extends Dictionary{
         boolean flag = false;
         Scanner scn = new Scanner(System.in);
         while (!flag) {
-            System.out.println("1: Add");
-            System.out.println("2: Show All");
-            System.out.println("3: Remove");
-            System.out.println("4: Close");
-            System.out.print("Option: ");
-            switch (scn.nextInt()) {
-                case 1: //add
-                    scn.nextLine();
+            System.out.println("    1: Add");
+            System.out.println("    2: Show All");
+            System.out.println("    3: Remove");
+            System.out.println("    4: Close");
+            System.out.print("  Option: ");
+            switch (scn.nextLine()) {
+                case "1": //add
                     System.out.print("Word: ");
                     String word = scn.nextLine();
                     System.out.print("Definition: ");
@@ -42,17 +38,16 @@ public class FavouriteDictionary extends Dictionary{
                     super.addWord(newWord);
                     save();
                     break;
-                case 2: //show all
+                case "2": //show all
                     super.print();
                     break;
-                case 3: //remove
+                case "3": //remove
                     System.out.print("Word to remove: ");
-                    scn.nextLine();
                     String wordToRemove = scn.nextLine();
                     super.deleteWord(wordToRemove);
                     save();
                     break;
-                case 4:
+                case "4":
                     flag = true;
                     save();
                     break;
@@ -62,9 +57,5 @@ public class FavouriteDictionary extends Dictionary{
                     break;
             }
         }
-    }
-
-    public void flashCard() {
-        //todo:
     }
 }
